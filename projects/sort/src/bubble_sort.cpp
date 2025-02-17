feac2faa76d4836c74dd595b13454028dda271a3
Vitaliy Suhoplechev <vitaliy.suhoplechev.1@gmail.com>
2025-02-17 15:55:30 +0500

    style: remove some comments

--- projects/sort/src/bubble_sort.cpp
+++ projects/sort/src/bubble_sort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "bubble_sort.hpp"
 
-// Реализация функции сортировки
 void bubble_sort(int* const arr, const int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
@@ -14,7 +13,6 @@ void bubble_sort(int* const arr, const int size) {
     }
 }
 
-// Реализация функции для печати массива
 void print_array(const char* const comment, int* arr, const int size) {
     const char format = ' ';
     std::cout << comment;
--- projects/sort/src/bubble_sort.hpp
+++ projects/sort/src/bubble_sort.hpp
@@ -1,10 +1,8 @@
 #ifndef BUBBLE_SORT_HPP
 #define BUBBLE_SORT_HPP
 
-// Объявление функции сортировки
 void bubble_sort(int* const arr, const int size);
 
-// Объявление функции для печати массива
 void print_array(const char* const comment, int* arr, const int size);
 
-#endif // BUBBLE_SORT_HPP
+#endif
